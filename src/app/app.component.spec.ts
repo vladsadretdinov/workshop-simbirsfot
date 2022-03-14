c244d3e2856d5ef7f5475e914cba269bff38181d
Vladislav Sadretdinov <vlad.sadretdinov@gmail.com>
2022-03-14 22:02:15 +0300

    Adjust linters

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'workshop-2'`, () => {
+  it("should have as title 'workshop'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('workshop-2');
+    expect(app.title).toEqual('workshop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('workshop-2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'workshop app is running!',
+    );
   });
 });
